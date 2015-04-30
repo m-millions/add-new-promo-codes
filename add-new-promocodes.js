f2df8bbddb9b4d28bcd80fddbe1b6695682b2504
Claudia Ventresca <civentresca@gmail.com>
2015-04-29 21:48:18 -0400

    Minor changes to script for style and reference - does not affect over-all functionality of project.

--- add-new-promocodes.js
+++ add-new-promocodes.js
@@ -1,11 +1,14 @@
 /*
-  node add-discount-codes.js 1 511 0 2015 05 01 2015 05 14
+  To run the script type, in the directory where teh app lives, type the
+  following: node add-promo-codes.js 1 511 0 2015 05 01 2015 05 14
+  - where "2015 05 01 2015 05 14" represents start and end dates.
   If promotion code is set with leading zeros it will truncate to the next whole
   number <<< entering '007' at command prompt, yeilds '7' in code
 */
 if (process.argv.length != 11){
     console.log("usage: node add-promo-codes.js num_codes \
-                        promotion_id start end unlimited");
+                        promotion_id start_year start_month start_day \
+                        end_year end_month end_day unlimited");
     return;
 }
 
@@ -16,6 +19,7 @@ var ShortId = require("shortid");
 var uristring = 'mongodb://localhost/picasso';
 mongoose.connect(uristring, function (err, res) {
   if (err) {
+    //TODO: Write exceptions and erros to log file(s)
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
     return;
   } else {
@@ -40,11 +44,11 @@ var num_codes = parseInt(process.argv.slice(2)),
     end_month = end_month - 1
     end_day = parseInt(process.argv.slice(10)),
     end = new Date(end_year, end_month, end_day)
-
     unlimited = parseInt(process.argv.slice(4)),
     updated_at = new Date(),
     created_at = new Date(),
     consumed = 0;
+// Remove persistance to the console at will
 console.log ('Successfully created and set PromoCode variables.');
 
 
@@ -59,6 +63,7 @@ var promocodeSchema = new mongoose.Schema({
     updated_at: Date,
     consumed: Number
 });
+// Remove persistance to the console at will
 console.log ('Successfully created promocodeSchema.');
 
 //This funciton actually creates and saves the new PromoCode
@@ -71,6 +76,7 @@ function addNewPromoCode(newPromoCode, promocodesi){
         updated_at: updated_at, created_at: created_at,
         consumed: consumed
     });
+    // Remove persistance to the console at will
     console.log ('Successfully created newPromoCode.');
     //Save newPromoCode to the database
     newPromoCode.save(function(err) {
@@ -83,12 +89,10 @@ function addNewPromoCode(newPromoCode, promocodesi){
 
 // Create PromoCode Model
 var PromoCode = mongoose.model('PromoCodes', promocodeSchema);
+// Remove persistance to the console at will
 console.log ('Successfully created PromoCode.');
 // Iterated through the number of promocodes to be created
 for (var i = 0; i < num_codes; i++){
     var promocodesi = ShortId.generate();
     addNewPromoCode(PromoCode, promocodesi);
 };
-// causes a false error "Error on save!MongoError: server localhost:27017
-// sockets closed" - but still saves record to the collection.
-// mongoose.connection.close();
